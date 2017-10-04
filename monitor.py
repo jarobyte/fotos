4b89516885f864543249f7a9a0ef29d6dcbb3787
Juan Ramirez <caballazo.loco@gmail.com>
2017-10-03 21:34:29 -0500

    monitor trabajando

--- monitor.py
+++ monitor.py
@@ -1,15 +1,41 @@
 from os import listdir
 
-contenidoActual = listdir("imagenes")
+carpeta = "imagenes"
+print("Monitoreando /" + carpeta)
+print()
+
+contenidoActual = listdir(carpeta)
 print("El contenido actual de la carpeta es:" + str(contenidoActual))
 
-registro = open("registro", "r")
-contenidoAnteriorCadena = registro.read()
+historial = open("historial", "r")
+contenidoAnteriorCadena = historial.read()
+historial.close()
 contenidoAnterior = contenidoAnteriorCadena.splitlines()
-print("El contenido anterior de la carpeta es:" + str(contenidoAnterior))
+print("El contenido anterior de la carpeta era:" + str(contenidoAnterior))
 
 def difference(a, b):
     return list(set(a).difference(set(b)))
 
 diff = difference(contenidoActual, contenidoAnterior)
 print("Los siguientes archivos no estaban:" + str(diff))
+
+def registrar(archivo):
+    registro = open("registro", "a")
+    registro.write(archivo + "\n")
+    print("Registrado como enviado " + archivo)
+    registro.close()
+
+def enviar(archivo):
+    print()
+    print("Enviando " + archivo)
+    print("Enviado  " + archivo)
+    registrar(archivo)
+    print()
+
+for i in diff:
+    enviar(i)
+
+nuevoHistorial = open("historial", "w")
+for i in contenidoActual:
+    nuevoHistorial.write(str(i) + "\n")
+nuevoHistorial.close()
